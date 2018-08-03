8bd02f6ff5e0db0f6ab809b72dfb80c788d20489
WatsonJay <446725026@qq.com>
2018-08-03 16:15:53 +0800

    修改显示

--- KuGou_Spid/Music_Search.py
+++ KuGou_Spid/Music_Search.py
@@ -6,7 +6,7 @@ def search(keyword):
     # 这里需要判断一下，ip与搜索字段可能会限制搜索，total进行判断
     total = web_request.parse(search_url)['data']['total']
     if total != 0:
-        search_total_url = search_url + '&pagesize=%d&platform=WebFilter' % total
+        search_total_url = search_url + '&pagesize=%d' % total
         music_list = web_request.parse(search_total_url)['data']['lists']
         item, items = {}, []
         for music in music_list:
@@ -14,9 +14,12 @@ def search(keyword):
                 item['Song'] = music['SongName']  # 歌名
                 item['Singer'] = music['SingerName']  # 歌手
                 item['Hash'] = music['SQFileHash'] # 歌曲无损hash
-                item['Size'] = str((music['SQFileSize']/1024)/1024)
+                item['Size'] = str(round((music['SQFileSize']/1024)/1024,2))+'M'
                 item['minute'] = str(int(music['SQDuration']/60))
-                item['second'] = str(music['SQDuration']%60)
+                if(music['SQDuration']%60<10):
+                    item['second'] = '0'+str(music['SQDuration'] % 60)
+                else:
+                    item['second'] = str(music['SQDuration'] % 60)
                 items.append(copy.deepcopy(item))
         return items
     else:
